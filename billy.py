8fe7c526aa422b95637a73d69a67d60a305c0365
Natalia <nataliamirandac@hotmail.com>
2020-09-06 15:37:45 -0400

    Two python files

---
+++ billy.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+print("Hi, " + name)
+favorite_color = input("What is your favorite color? ")
+if(favorite_color == "Blue"):
+    print("You suck")
+else:
+        print("that is acceptable")
---
+++ one.py
@@ -0,0 +1,23 @@
+# name = "John"
+# age = 40
+# is_new = True
+# print(name)
+
+
+#build a input - store it to variable - print the result
+# name = input("What is your name? ")
+# favoriteColor = input("What is your favorite color? ")
+# print("Hi, " + name )
+
+# if(favoriteColor == "Blue"):
+#        print("Blue is a great color") 
+# else:
+#         print("That is a terrible choice of color") 
+
+# print(name + " likes " + favoriteColor)
+
+#-----------------------------------------------------------------//---------------------
+
+weight = input("How much do you weight in pounds? ")
+kg = int(weight) / 2.2046
+print("Your weight in Kg is: " + str(kg))
